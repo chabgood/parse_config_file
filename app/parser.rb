e173052dedee9a375ce71e1bf06d791d1221e3a3
chris habgood <chabgood@gmail.com>
2015-02-12 18:48:19 -0800

    valid and invalid file tests work

--- app/parser.rb
+++ app/parser.rb
@@ -1,14 +1,49 @@
 class Parser
+  attr_accessor :results, :errors, :file
 
   def initialize(args={})
     raise 'A file must be supplied' if args[:file].nil?
-    @file = args[:file]
+    @file = File.open(args[:file])
+    @results = []
+    @errors = []
   end
 
   def parse
-    @file.each do |line|
-      p line
+    file.each do |line|
+      line = line.strip  # removes newline
+      next if a_comment?(line) || error_if_invalid_line(line)
+      results << get_line_result(line)
+    end
+    self
+  end
+
+  private
+
+  def get_line_result(line)
+    stripped_line = line.split('=').last.strip
+    return true if stripped_line =~ /(yes|true|on)/
+    return false if stripped_line =~ /(no|false|off)/
+    stripped_line
+  end
+
+  def a_comment?(line)
+    line.start_with?('#')
+  end
+
+  def error_if_invalid_line(line)
+    if invalid_line?(line)
+      errors << line
+      return true
     end
   end
 
+  def invalid_line?(line)
+    stripped_line = line.split('=').each(&:strip)
+    stripped_line.length < 2 || line !~ /=/
+  end 
+
 end
+
+parser = Parser.new(:file => '/home/chabgood/projects/parse_config_file/spec/invalid_file.txt')
+parser.parse
+p parser.inspect
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,18 +8,45 @@ describe Parser do
   end
 
   it 'should upload a file' do
-    Parser.new(:file => 'test_file.txt')
+    Parser.new(:file => '/home/chabgood/projects/parse_config_file/spec/test_file.txt')
   end
 
-  context "Parse config file" do
+  context "#parse" do
+    describe "valid file" do
+      let(:new_parser) {Parser.new(:file => '/home/chabgood/projects/parse_config_file/spec/test_file.txt')}
+      let(:parsed_file) {new_parser.parse}
 
-    before(:all) do
-      parser = Parser.new(:file => 'test_file.txt')
-      parser.parse
+      it "should have a result count of 9" do
+        expect(parsed_file.results.length).to eq(9)
+      end
+
+      it "should ignore comments" do
+        size = File.foreach('/home/chabgood/projects/parse_config_file/spec/test_file.txt').inject(0) {|c, line| c+1}
+        expect(size).to eq(12)
+      end
+
+      it "should have no errors" do
+        expect(parsed_file.errors.length).to eq(0)
+      end
+
+      it "should match the results" do
+        expect(parsed_file.results).to eq(['test.com','55331','2.5','user',true,true,false,'/tmp/logfile.log',true])
+      end
     end
+  end
 
-    it "should ignore comments" do
-      
+  context "#parse" do
+    describe "invalid file" do
+      let(:new_parser) {Parser.new(:file => '/home/chabgood/projects/parse_config_file/spec/invalid_file.txt')}
+      let(:parsed_file) {new_parser.parse}
+
+      it "should have a result count of 7" do
+        expect(parsed_file.results.length).to eq(7)
+      end
+
+      it "should have 2 errors" do
+        expect(parsed_file.errors.length).to eq(2)
+      end
     end
   end
 
